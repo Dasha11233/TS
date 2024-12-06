a99436d0076ba985cbd25c2e545638afc85990d3
Dasha11233 <131310629+Dasha11233@users.noreply.github.com>
2024-12-06 12:47:03 +0300

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const messageElement = document.getElementById('message');
+    if (messageElement) {
+        messageElement.textContent = 'Hello TypeScript!';
+    } else {
+        console.error('Element with id "message" not found.');
+    }
+});
